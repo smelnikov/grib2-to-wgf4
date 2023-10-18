7f2a3aa03a1deb13527486cebbb230cb4d3488a1
Sergei Melnikov <smelnikov@uniqsoft.ae>
2023-10-18 22:31:50 +0400

    Building the output file is not entirely correct

---
+++ grib2wgf4/__init__.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass
+from typing import Protocol, SupportsIndex
+
+import numpy as np
+from numpy import ma
+
+MISSING_VALUE = -100_500.0
+PRECISION_DECIMALS = 6
+PRECISION_SCALE = 10**PRECISION_DECIMALS
+
+# fmt: off
+class _IOProtocol(Protocol):
+    def flush(self) -> object: ...
+    def fileno(self) -> int: ...
+    def tell(self) -> SupportsIndex: ...
+    def seek(self, offset: int, whence: int, /) -> object: ...
+# fmt: on
+
+
+@dataclass
+class FileHeader:
+    min_lat: int
+    max_lat: int
+    min_lon: int
+    max_lon: int
+    step_lat: int
+    step_lon: int
+    scale: int
+
+    @classmethod
+    def from_latlons(cls, lats: np.ndarray, lons: np.ndarray) -> "FileHeader":
+        lats, lons = lats.round(PRECISION_DECIMALS), lons.round(PRECISION_DECIMALS)
+        min_lat = int(lats.min() * PRECISION_SCALE)
+        max_lat = int(lats.max() * PRECISION_SCALE)
+        min_lon = int(lons.min() * PRECISION_SCALE)
+        max_lon = int(lons.max() * PRECISION_SCALE)
+        step_lat = (max_lat - min_lat) // (lats.shape[0] - 1)
+        step_lon = (max_lon - min_lon) // (lons.shape[1] - 1)
+
+        return cls(
+            min_lat, max_lat, min_lon, max_lon, step_lat, step_lon, PRECISION_SCALE
+        )
+
+    def tofile(self, fid: _IOProtocol) -> None:
+        np.array(
+            [
+                self.min_lat,
+                self.max_lat,
+                self.min_lon,
+                self.max_lon,
+                self.step_lat,
+                self.step_lon,
+                self.scale,
+            ],
+            dtype="i",
+        ).tofile(fid)
+
+
+@dataclass
+class GridValues:
+    values: ma.MaskedArray
+
+    @classmethod
+    def from_accum(
+        cls,
+        curr: ma.MaskedArray,
+        prev: ma.MaskedArray | None = None,
+    ) -> "GridValues":
+        pure_values = curr
+        if prev is not None:
+            # TODO: check that subtraction is correct for masked values
+            pure_values = curr - prev
+        return cls(pure_values)
+
+    def tofile(self, fid: _IOProtocol) -> None:
+        self.values.filled(MISSING_VALUE).astype(np.float32).tofile(fid)
+
+
+@dataclass
+class FileBuilder:
+    header: FileHeader
+    values: GridValues
+    delimiter = np.array([MISSING_VALUE], dtype="f")
+
+    def tofile(self, fid: _IOProtocol) -> None:
+        self.header.tofile(fid)
+        self.delimiter.tofile(fid)
+        self.values.tofile(fid)
--- grib2wgf4/__main__.py
+++ grib2wgf4/__main__.py
@@ -1,16 +1,23 @@
+import struct
+
 import pygrib
+
+from grib2wgf4 import FileBuilder, FileHeader, GridValues
 
 if __name__ == "__main__":
     grbs = pygrib.open(
-        "./data/icon-d2_germany_regular-lat-lon_single-level_2023101712_000_2d_tot_prec.grib2"
+        "./samples/icon-d2_germany_regular-lat-lon_single-level_2023101712_000_2d_tot_prec.grib2"
     )
+    grb = grbs.select(shortName="tp")[0]
 
-    grbs.seek(0)
+    values, lats, lons = grb.data()
 
-    for grb in grbs.select(shortName="tp"):
-        print(grb)
+    with open("output/temp.wgf4", mode="wb") as fid:
+        FileBuilder(
+            FileHeader.from_latlons(lats, lons),
+            GridValues.from_accum(values, prev=None),
+        ).tofile(fid)
 
-        values, lats, lons = grb.data()
-
-        print(values.shape)
-        print(lats.shape, lats.min(), lats.max(), lons.shape, lons.min(), lons.max())
+    with open("output/temp.wgf4", mode="rb") as fid:
+        print(struct.unpack("i" * 7 + "f", fid.read(32)))
+        print(struct.unpack("f" * 8, fid.read(32)))
