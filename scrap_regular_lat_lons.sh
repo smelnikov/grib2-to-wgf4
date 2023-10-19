77b060014921b5f4af68cc128c48b8d5b0030504
Sergei Melnikov <smelnikov@uniqsoft.ae>
2023-10-19 21:07:17 +0400

    Scrap files via bash script

---
+++ scrap_regular_lat_lons.sh
@@ -0,0 +1,44 @@
+#
+Usage()
+{
+echo "usage: scrap_regular_lat_lons [-h] [source] [outdir]
+
+Scraps every *.regular*.grib2.bz2 file from the source url
+
+positional arguments:
+  source    source URL
+  outdir    path to output directory, it will be created if doesn't exists.
+
+
+options:
+  -h        show this help message and exit"
+}
+
+while getopts ":h" option; do
+   case "$option" in
+      h) 
+        Usage
+        exit ;;
+   esac
+done
+
+if [ -z "$1" ]
+then
+  echo "error: the following arguments are required: source"
+fi
+if [ -z "$2" ]
+then
+  echo "error: the following arguments are required: outdir"
+  exit 1
+fi
+
+(
+mkdir -p $2 && cd $2;
+curl -s $1 \
+  | tr '"' '\n' \
+  | tr "'" '\n' \
+  | grep -e 'icon-.\+regular.\+grib2.bz2$' \
+  | xargs -I% -P10  bash -c "curl -sO -C - '$1%' && echo %" \
+  | xargs -I% -P10 bash -c "bunzip2 -f -k % && echo $PWD'/%' | sed 's/.bz2//'" \
+  | sort
+)
